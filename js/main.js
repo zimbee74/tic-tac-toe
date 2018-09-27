47556c82ec16ff37c999a2a41b1620991c1e7de3
Brian Ellis <ellisbrian74@gmail.com>
2018-09-28 09:52:50 +1000

    trying to complete jquery version now

--- js/main.js
+++ js/main.js
@@ -6,33 +6,27 @@ let currentTurn = 'X';
 let images = [];
 let xMoves = "";
 let oMoves = "";
-// let hasBeenWonSoStop ="";
 let playGame = true;
 
 window.onload = function() {
-
   images = document.getElementsByClassName('imageSwitcher');
-
   // this for loop attaches the same click handler to every game square (with the .imageSwitcher class)
   for (let i = 0; i < images.length; i++ ) {
     console.log(images[i]);
-
-
-//_____________________________________________________________________________
     images[i].addEventListener('click', function( event ){
-      if (playGame) {
+      if (playGame) { // nested if statements with local variable
+        // tells program if play game true run code till LINE 45
+        // program then runs till LINE 58 for winner check
         let eti = event.target.id;
-
         // should we accept this click and play the move, or is it an invalid click,
         // in which case we should ignore it
         if( !event.target.src.endsWith('BLANK.png') ){
-          // ignore the click by returning early, if the square is NOT blank
+          // ignore the click by RETURN early, if the square is NOT blank
+          // blank state is default state set in HTML
           return;
         }
-
          event.target.src = `images/TIC_TAC_TOE_${currentTurn}.png`;
-
-
+         // Tells program to flip image by changing SRC name on click
         if (currentTurn === 'X') {
           // X just had its turn
           xMoves += eti;
@@ -43,105 +37,51 @@ window.onload = function() {
           oMoves += eti;
           pickWinnerFunction( oMoves );
           currentTurn = 'X';
-        }
-      }
-//
+        } // if statement CHILD that checks if blank
+      } // if statement PARENT that checks if blank
+        // ensures non blank squares can be clicked
+        // makes first use of currentTurn variable to track who's move
 
     }); // end of click handler function
-    // }
-//___________________________________________________________________________
-  } // for
-
-  // FUNCTION NEEDS TO GO TO START DUPLICATE ABOVE and BELOW
-  // FUNCTION NEEDS TO GO TO START
-  const pickWinnerFunction = function( moveHistory ) {
-
-    // if (currentTurn) {
-    //   this.src = "images/TIC_TAC_TOE_X.png";
-    //   currentTurn = false;
-    // } else {
-    //   this.src = "images/TIC_TAC_TOE_O.png";
-    //   currentTurn = true;
-    // }
-    // check for a win
-    // checkForWin();
-    // ______________________________________________________________________
-
+  } // for loop
+  const pickWinnerFunction = function( moveHistory ) { // has parameter move history
     const winningStrings = ['abc', 'def', 'ghi', 'adg', 'beh', 'cfi', 'aei', 'ceg'];
-
-    for (let i = 0; i < winningStrings.length; i++) {
-      const letters = winningStrings[i].split('');
-      // how does this connect
-      const first = letters[0];
-      const second = letters[1];
-      const third = letters[2];
-      // console.log(letters[0]);
-      // console.log(letters[1]);
-      // console.log(letters[2]);
-      // const compare = enteredStrings.includes(winningStrings[i]);
-      let possibleWinner = moveHistory.includes(first)  &&
-                    moveHistory.includes(second) &&
-                    moveHistory.includes(third);
-
-
+    // above is hard coded winning string combinations
+    for (let i = 0; i < winningStrings.length; i++) { //for loop keeps count
+      const letters = winningStrings[i].split(''); //captures enetered items in array
+      const first = letters[0];  // creates variable for easy handling
+      const second = letters[1]; // creates variable for easy handling
+      const third = letters[2]; //  creates variable for easy handling
+      let possibleWinner =  moveHistory.includes(first)  && // check
+                            moveHistory.includes(second) && // check
+                            moveHistory.includes(third); // check
       if( possibleWinner ){
         document.getElementsByTagName("h1")[0].innerHTML = `${currentTurn} HAZ WON`;
+        playGame = false; // stops game by disabling listner on blank squares
+      } else if( xMoves.length + oMoves.length === 9 ){
+        // possibleWinner is false
+        document.getElementsByTagName("h1")[0].innerHTML = `IZ A TIE`;
         playGame = false;
-        // console.log('WINNER!',  winningStrings[i]);
-        // document.getElementsByClassName("row").style.pointerEvents='none';
-        // document.getElementsByClassName("row").removeEventListener("mousemove", window);
-
-        //console.log((xMoves + oMoves).length);
-        // document.getElementById(id).style.property = new style
       }
 
-    }
+      // runs logic that announces winner and stops clicking of squares
+    }   // splits winning strings into an array
+  };    // pickWinnerFunction
+        // - runs check against the 9 possible hard coded combinations
+        // if check is false executes the below code
 
-  }; // pickWinnerFunction
-
-
-
-// const identifyDrawFunction = function() {
-  //  {
-  // if (true) {
-  //   else if (xMoves.length + oMoves.length >= 9 && winningStrings[i] === false)
-  // }
-
-         // document.getElementsByTagName("h1")[0].innerHTML = `IT'S A TIE`;
-  // }
-
-// this is where you put the on click reset the program
-// i.e. set the squares to BLANK
-
-
-const h2 = document.querySelector('h2');
-h2.addEventListener('click', function( event ){
-  console.log('clicked reset!');
-
-  xMoves = "";
-  oMoves = "";
-  playGame = true;
+const h2 = document.querySelector('h2'); // select H2 tag
+h2.addEventListener('click', function( event ){ // add event lister for click
+  xMoves = ""; // empties
+  oMoves = ""; // empties
+  playGame = true; // resets ability to play game to true - enables new game
+  currentTurn = 'X'; // ensures X always starts remove to alternate
   document.getElementsByTagName("h1")[0].innerHTML = `TIC-TAC-TOE`;
 
   for (let i = 0; i < images.length; i++ ) {
     console.log(images[i].src);
     images[i].src = 'images/TIC_TAC_TOE_BLANK.png';
   }
-  //
-  // if (playGame) {
-  //   let reset = event.target.h2;
-  //
-  //           // should we accept this click and play the move, or is it an invalid click,
-  //           // in which case we should ignore it
-  //   // if( !event.target.src.endsWith('BLANK.png') ){
-  //   //         // ignore the click by returning early, if the square is NOT blank
-  //   //         //return;
-  //   // }
-  //
-  //    event.target.src = `images/TIC_TAC_TOE_BLANK.png`;
-  // }
 });
-
-
 
 }; // Window on load function
